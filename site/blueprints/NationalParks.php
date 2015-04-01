d199df800acb57ec98e64bc446ec1977dfe293ad
August Miller <hello@gusmiller.com>
2015-04-01 11:39:07 -0700

    Indentation issues, phone field type

--- site/blueprints/NationalParks.php
+++ site/blueprints/NationalParks.php
@@ -4,50 +4,50 @@ title: National Parks
 pages: false
 files: true
 fields:
-    title:
-      label: Park Name
-      type:  text
+  title:
+    label: Park Name
+    type:  text
 
-    area: 
-      label: Area in Square Miles
-      type: number
+  area: 
+    label: Area in Square Miles
+    type: number
 
-    location:
-      label: location
-      type: select
-      options:
-        oregon: Oregon
-        washington: Washington
-        utah: Utah
-        maine: Maine
+  location:
+    label: location
+    type: select
+    options:
+      oregon: Oregon
+      washington: Washington
+      utah: Utah
+      maine: Maine
 
-    history:
-      label: History of the National Parks
-      type: textarea
+  history:
+    label: History of the National Parks
+    type: textarea
 
-    phone:
-      label: Phone
-      type: phone
+  phone:
+    label: Phone
+    type: tel
 
-    wildlife:
-      label: Wildlife Encounters
-      type: text area
+  wildlife:
+    label: Wildlife Encounters
+    type: textarea
 
-    season:
-      label: season
-      type: select
-      options:
-        summer: Summer
-        fall: Fall
-        winter: Winter
-        spring: Spring
+  season:
+    label: Season
+    type: select
+    options:
+      summer: Summer
+      fall: Fall
+      winter: Winter
+      spring: Spring
 
-    jobs:
-      label: Jobs Available
-      type: select
-      options:
-        wrangler: Wrangler
-        giftshop: Gift Shop
-        server: Server
-        housing: Housing
-  
+  jobs:
+    label: Jobs Available
+    type: checkboxes
+    options:
+      wrangler: Wrangler
+      giftshop: Gift Shop
+      server: Server
+      housing: Housing
+
