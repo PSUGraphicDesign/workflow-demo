259bf8e2f48cfd85c7c959f154939fbb7593f958
August Miller <hello@gusmiller.com>
2015-04-01 11:04:32 -0700

    Add Digital Servies blueprint

---
+++ site/blueprints/digital-services.php
@@ -0,0 +1,26 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Digital Services
+pages: false
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+
+  website:
+    label: Link
+    type: url
+
+  user_count:
+    label: Number of Active Users
+    type: number
+
+  history:
+    label: A Brief History of the Service
+    type: textarea
+
+  date_founded:
+    label: Date Founded
+    type: date
+
