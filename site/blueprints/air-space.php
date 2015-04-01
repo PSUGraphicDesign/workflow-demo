26c2351969425e21f49fe3b41e0944d25aa3d0f4
Bill Phan <billphan25@gmail.com>
2015-04-01 11:04:42 -0700

    Add Air Space Blueprint

---
+++ site/blueprints/air-space.php
@@ -0,0 +1,31 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Air Space
+pages: false
+files: true
+fields:
+  title:
+    label: Title
+    type:  text
+
+  website:
+  	lable: Link
+  	type: url
+
+  history:
+    label: History
+    type: textarea
+
+  specifications:
+    label: Specifications
+    type: textarea
+
+  first_flight:
+    label: First Flight
+    type: date
+
+  more_info:
+    label: More Information
+    type: url
+
+
