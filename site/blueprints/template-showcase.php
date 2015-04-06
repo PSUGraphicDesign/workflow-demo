e2c48081429d662612bb80d66827e18527acc4cc
August Miller <hello@gusmiller.com>
2015-04-05 23:01:14 -0700

    Add blueprint for template showcase

---
+++ site/blueprints/template-showcase.php
@@ -0,0 +1,14 @@
+<?php if(!defined('KIRBY')) exit ?>
+
+title: Template Showcase Group
+pages: true
+files: false
+fields:
+  title:
+    label: Template Showcase Name
+    type:  text
+
+  intro:
+    label: Description
+    type: textarea
+    help: Describe this group of templates.
